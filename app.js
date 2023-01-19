38d2fe37db0709324e343cd504e740492103500a
srinivas-yerramsetty <srinivasyerramsetty91@gmail.com>
2023-01-19 10:59:56 +0000

    removed Error

--- app.js
+++ app.js
@@ -79,5 +79,4 @@ app.post("/movies/", async (request, response) => {
   let dbResult = await db.run(insertQuery);
   const movieId = dbResult.lastID;
   response.send("Movie Successfully Added");
-  //getting an Error
 });
